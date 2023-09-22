d2ae8b64081e696fceab8a37d3599fea57fd1b42
Nick Whitelegg <nickw4426@gmail.com>
2023-09-22 13:50:15 +0100

    modify webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,25 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-	entry: './src/main.js',
-	devtool: 'inline-source-map',
-	devServer: {
-		static: './dist',
-	},
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-		clean: true
-	},
-	module: {
-		rules: [
-			{ test: /\.css/i, use: ['style-loader', 'css-loader'] },
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: 'asset/resource',
-			},
+    entry: './src/main.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            // If it's CSS, process using CSS loaders
+            { test: /\.css/i, use: ['style-loader', 'css-loader'] },
+            // If it's .jpg, .png or .gif, process as a resource
+            {
+                test: /\.(png|jpg|gif)$/i,
+                type: 'asset/resource',
+            },
 
-		]
-	},
-	// plugins: [
-	// 	new HtmlWebpackPlugin({
-	// 		title: 'To Do List',
-	// 	}),
-	// ],
-	// optimization: {
-	// 	runtimeChunk: 'single',
-	// },
+        ]
+    },
 }
